8c11144f6494ce2b34fa355776f372c06182f27c
John Hill <john.m.hill91@gmail.com>
2019-01-13 02:38:53 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars=['snikers','hundred grand','kitkat','skittles'];
+function addElementToBeginningOfArray(array, element){
+ element.unshift(array)
+}
+function destructivelyAddElementToBeginningOfArray(array,element){
+  element...array
+}
+function addElementToEndOfArray(array,element){
+  element.push(array)
+}
+function destructivelyAddElementToEndOfArray(array,element){
+array...element
+}
