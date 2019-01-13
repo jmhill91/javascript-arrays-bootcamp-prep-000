5d029a59435567c99341113251b349bf2bab2876
John Hill <john.m.hill91@gmail.com>
2019-01-13 02:59:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,6 @@ var chocolateBars=['snikers','hundred grand','kitkat','skittles'];
  function addElementToBeginningOfArray(array,element){
   return [element, ...array]
  }
+function destructivelyAddElementToBeginningOfArray(arry,element){
+  return[]
+}
