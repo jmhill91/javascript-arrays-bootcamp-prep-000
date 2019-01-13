786c8aa03dad2cb4f0d004343ebeae6595b47a72
John Hill <john.m.hill91@gmail.com>
2019-01-13 03:07:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,6 +10,6 @@ function addElementToEndOfArray(array,element){
   return [array,...element]
 }
 function destructivelyAddElementToEndOfArray(array,element){
-  array.puch(element)
+  array.push(element)
   return array
 }
