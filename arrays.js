2dfe81b71729f8cc8554fbb0a6ed53b70551541b
John Hill <john.m.hill91@gmail.com>
2019-01-13 02:52:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
 var chocolateBars=['snikers','hundred grand','kitkat','skittles'];
  function addElementToBeginnigofArray(array,element){
-   choclateBars.unshift(element)
+   element...chocolateBars
  }
