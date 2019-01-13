1cbdf273773b10d1a5e26c246440c3327dd39e66
John Hill <john.m.hill91@gmail.com>
2019-01-13 03:17:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -16,3 +16,6 @@ function destructivelyAddElementToEndOfArray(array,element){
 function accessElementInArray(array,index){
   return array[index]
 }
+function destructivelyRemoveRlementFromBeginningOfArray(array){
+  
+}
